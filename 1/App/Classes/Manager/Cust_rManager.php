3ab022e5a2aeccc48c56afa2057b2986f893d882
aytac23 <sertac.aytac@yahoo.fr>
2017-09-24 20:04:55 +0200

    creation de l'utilisateur

--- 1/App/Classes/Manager/Cust_rManager.php
+++ 1/App/Classes/Manager/Cust_rManager.php
@@ -17,9 +17,65 @@ class Cust_rManager implements \MyInterface\crud
 
     }
 
-    public function insert($obj)
+
+    public function insert($cust_r)
     {
-        // TODO: Implement insert() method.
+
+        $query = "insert into utilisateur (mdp, name, pname, dtb, mail, adress, cp, city, country, type, actif) "
+            . "values (:mdp, :name, :pname, :dtb, :mail, :adress, :cp, :city, :country, :type, :actif)";
+
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':mdp', $cust_r->getMdp(), \PDO::PARAM_STR);
+        $stmt->bindParam(':name', $cust_r->getName(), \PDO::PARAM_STR);
+        $stmt->bindParam(':pname', $cust_r->getPname(), \PDO::PARAM_STR);
+        $stmt->bindParam(':dtb', $cust_r->getDtb(), \PDO::PARAM_STR);
+        $stmt->bindParam(':mail', $cust_r->getMail(), \PDO::PARAM_STR);
+        $stmt->bindParam(':adress', $cust_r->getAdress(), \PDO::PARAM_STR);
+        $stmt->bindParam(':cp', $cust_r->getCp(), \PDO::PARAM_STR);
+        $stmt->bindParam(':city', $cust_r->getCity(), \PDO::PARAM_STR);
+        $stmt->bindParam(':country', $cust_r->getCountry(), \PDO::PARAM_STR);
+        $stmt->bindParam(':type', $cust_r->getType(), \PDO::PARAM_STR);
+        $stmt->bindParam(':actif', $cust_r->getActif(), \PDO::PARAM_BOOL);
+        $stmt->execute();
+
+        $cust_r->setId_cli($this->pdo->lastInsertId());
+    }
+
+    public function emailExist ($mail){
+        if (!isset($mail) || $mail==""){
+            return false;
+        }
+
+        $query = "SELECT * FROM cust_r WHERE mail=:mail";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(":mail", $mail, \PDO::PARAM_STR);
+        $stmt->execute();
+
+        if($stmt->rowCount()>0){
+            return true;
+        }
+
+        return false;
+    }
+
+    public function authentification ($mail, $mdp){
+
+        if (!isset($mail) || $mail=="" || !isset($mdp) || $mdp==""){
+            return false;
+        }
+
+        $query ="SELECT * FROM cust_r WHERE mail =:mail AND mdp=:mdp";
+        $stmt=$this->pdo->prepare($query);
+        $stmt->bindParam(":mail", $mail, \PDO::PARAM_STR);
+        $stmt->bindParam(":mdp", $mdp, \PDO::PARAM_STR);
+        $stmt->execute();
+
+        if ($stmt->rowCount() !=1){
+            return false;
+        }else{
+
+        }
+
     }
 
     public function selectAll()
